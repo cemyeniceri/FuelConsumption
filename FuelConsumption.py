4f6b9c9aedea38533f42276f4c5a1579af88a5aa
cemyeniceri <cem.yenicer@gmail.comi>
2019-02-06 18:57:26 +0300

    refactoring

--- FuelConsumption.py
+++ FuelConsumption.py
@@ -1,4 +1,4 @@
-import numpy
+import numpy as np
 import pandas
 from keras.layers import Dense
 from keras.models import Sequential
@@ -13,9 +13,7 @@ from sklearn.preprocessing import StandardScaler
 # Variables
 dataFrame = pandas.read_csv("dataset1.csv", delimiter=",", header=None,
                             usecols=[2, 4, 6, 8, 10, 12, 14, 18, 20, 22, 24])
-# df_reorder = dataFrame[['A', 'B', 'C', 'D', 'E']] # rearrange column here
 
-# dataFrame = pandas.read_csv("teoman.csv", delimiter=",", header=None)
 dataset = dataFrame.values
 
 X = dataset[:, [0, 1, 2, 3, 4, 5, 6, 8, 9, 10]]
@@ -29,14 +27,15 @@ def baseline_model():
     model.add(Dense(10, input_dim=10, kernel_initializer='normal', activation='relu'))
     model.add(Dense(1, kernel_initializer='normal'))
     # Compile model
-    model.compile(loss='mean_squared_error', optimizer='adam')
+    model.compile(loss='mse', optimizer='adam', metrics=['mse', 'mae'])
+
     model.summary()
     return model
 
 
 # fix random seed for reproducibility
 seed = 7
-numpy.random.seed(seed)
+np.random.seed(seed)
 # evaluate model with standardized dataset
 estimator = KerasRegressor(build_fn=baseline_model, epochs=100, batch_size=5, verbose=0)
 
@@ -44,7 +43,7 @@ kFold = KFold(n_splits=10, random_state=seed)
 results = cross_val_score(estimator, X, Y, cv=kFold)
 print("Results: %.2f (%.2f) MSE" % (results.mean(), results.std()))
 # evaluate model with standardized dataset
-numpy.random.seed(seed)
+np.random.seed(seed)
 estimators = [('standardize', StandardScaler()),
               ('mlp', KerasRegressor(build_fn=baseline_model, epochs=50, batch_size=5, verbose=0))]
 pipeline = Pipeline(estimators)
@@ -65,7 +64,7 @@ def larger_model():
     return model
 
 
-numpy.random.seed(seed)
+np.random.seed(seed)
 estimators = [('standardize', StandardScaler()),
               ('mlp', KerasRegressor(build_fn=larger_model, epochs=50, batch_size=5, verbose=0))]
 pipeline = Pipeline(estimators)
@@ -84,7 +83,7 @@ def wider_model():
     return model
 
 
-numpy.random.seed(seed)
+np.random.seed(seed)
 estimators = [('standardize', StandardScaler()),
               ('mlp', KerasRegressor(build_fn=wider_model, epochs=100, batch_size=5, verbose=0))]
 pipeline = Pipeline(estimators)
